5dff3444603a498701bf596e668d8e6f32d2a63e
davebeauchemin <37642902+davebeauchemin@users.noreply.github.com>
2019-02-15 16:35:59 -0500

    Updated

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,20 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
   // Strucutre by Wes Bos
   // - State
   // - Lifecycle Event
-  // - Custom Functions
+  // - Custom Methods/Functions
+
+  // State
   state = {
     fishes: {},
     order: {}
   };
 
+  static propTypes = {
+    match: PropTypes.object
+  };
+
+  // Lifecycle Event
+  componentDidMount() {
+    const { params } = this.props.match;
+    // 1. Reinstate our localstorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
+
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+    console.log("unmonted");
+  }
+
+  // Custom Methods/Function
   addFish = fish => {
     // 1. Take a copy of the existing state
     const fishes = { ...this.state.fishes };
@@ -27,6 +63,24 @@ class App extends React.Component {
     });
   };
 
+  updateFish = (key, updatedFish) => {
+    // 1. Take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    // 2. Update that state
+    fishes[key] = updatedFish;
+    // 3. Set that to state
+    this.setState({ fishes });
+  };
+
+  deleteFish = key => {
+    // 1. Take a copy of state
+    const fishes = { ...this.state.fishes };
+    //. 2. Update the state
+    fishes[key] = null;
+    // 3. Update state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -37,9 +91,16 @@ class App extends React.Component {
     // 2. Either add to the order or update the number in our order
     order[key] = order[key] + 1 || 1; // This will check if Fish[key] already exist in our order, if yes, it will add +1 if not, it will add it as 1.
     // 3. Call setState to update our state object
-    this.setState({
-      order
-    });
+    this.setState({ order });
+  };
+
+  removeFromOrder = key => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Remove that item from order
+    delete order[key];
+    // 3. Call setState to update our state object
+    this.setState({ order });
   };
 
   render() {
@@ -58,10 +119,18 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
+          storeId={this.props.match.params.storeId}
         />
       </div>
     );
