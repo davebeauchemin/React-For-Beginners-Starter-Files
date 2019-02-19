ca548fea497e2e60353249006e2b081d3a6042d6
davebeauchemin <37642902+davebeauchemin@users.noreply.github.com>
2019-02-19 13:06:47 -0500

    Finished

---
+++ catch-of-the-day/build/precache-manifest.6ea176493a546dc8633121130fdc1aee.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f58ecf6c300a52bc460",
+    "url": "/static/css/main.6b2177da.chunk.css"
+  },
+  {
+    "revision": "2f58ecf6c300a52bc460",
+    "url": "/static/js/main.2f58ecf6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "31a28c970adc01e37d98",
+    "url": "/static/js/2.31a28c97.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "f11cf2dcc7876acbadf3c93d96290277",
+    "url": "/index.html"
+  }
+];
